e204b20385f82ddbaa51adac8385a5e28ee3ae56
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-10-25 14:04:24 -0500

    Haciendo el ServletControler más ordenado en cuanto a métodos respecta ya que se a creado el método accionDefault ya que esté va a ser utilizado dentro del uso de métodos como doPost, y doGet

--- src/main/java/web/ServletControlador.java
+++ src/main/java/web/ServletControlador.java
@@ -22,12 +22,22 @@ public class ServletControlador extends HttpServlet{
     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        this.accionDefault(request, response);  // se manda a llamar al método accionDefault
+    } 
+    
+    /* 
+        Se corto el fragmento de código anterior por que estos scripts van a ser utilizados en otros métodos y para 
+        que no haya redundancia se crea mejor otro método
+    */
+    private void accionDefault(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
         List<Cliente> clientes = new ClienteDaoJDBC().listar();
         System.out.println("clientes = " + clientes);
-        request.setAttribute("clientes", clientes);                                  // con el alcance request se extiende el listado de clientes
-        request.setAttribute("totalClientes", clientes.size());                     // este dato se va a mandar hacia las vistas para obtener el total de clientes registrados
-        request.setAttribute("saldoTotal", this.calcularSaldoTotal(clientes));     // este dato se va a mandar hacia las vistas para obtener el saldoTotal de todos los clientes
-        request.getRequestDispatcher("clientes.jsp").forward(request, response);  // se envia la información al archivo clientes.jsp
+        HttpSession sesion = request.getSession();                                   // comoel alcance request no funciona se debe utilizar el sesion para que estos valores que se envia se puedan utilizar de forma mas eficiente
+        sesion.setAttribute("clientes", clientes);                                  // con el alcance request se extiende el listado de clientes
+        sesion.setAttribute("totalClientes", clientes.size());                     // este dato se va a mandar hacia las vistas para obtener el total de clientes registrados
+        sesion.setAttribute("saldoTotal", this.calcularSaldoTotal(clientes));     // este dato se va a mandar hacia las vistas para obtener el saldoTotal de todos los clientes
+        // request.getRequestDispatcher("clientes.jsp").forward(request, response);   se envia la información al archivo clientes.jsp // cuando se produce la acción de insertar la información se queda ahí y en caso de que se recarge la página la información se vuelve a insertar en la base de datos t produce un doble registro(para mas informacion ver el curso Academia java - De cero a experto con Ejercicios, Sección 66 Servlets y JSP´s: Proyecto Final Control Clientes, video número 254 minuto 17)
+        response.sendRedirect("clientes.jsp");  
     }
     
     /* método para calcular el saldo total de todos los clientes registrados */
@@ -37,5 +47,44 @@ public class ServletControlador extends HttpServlet{
             saldoTotal += cliente.getSaldo();
         }
         return saldoTotal;
+    }
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+       String accion = request.getParameter("accion");   // para obtener el parámetro obtenido desde los formularios de inserción y actualización
+       if(accion != null){
+           switch (accion) {
+               case "insertar":
+                   this.insertarCliente(request, response);
+                   break;
+               
+               default:
+                   this.accionDefault(request, response);
+           }
+       }
+       else{    // en caso de que la acción sea igual a null
+           this.accionDefault(request, response);
+       }
+    }
+    
+    private void insertarCliente(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        // recuperamos los valores del formulario agregarCliente
+        String nombre = request.getParameter("nombre");
+        String apellido = request.getParameter("apellido");
+        String email = request.getParameter("email");
+        String telefono = request.getParameter("telefono");
+        double saldo = 0;                                   // en caso de que no se envie ningun valor se ponga 0 por defecto
+        String saldoString = request.getParameter("saldo");
+        
+        if(saldoString != null & !"".equals(saldoString)){
+            saldo = Double.parseDouble(saldoString);
+        }
+        
+        Cliente cliente = new Cliente(nombre, apellido, email, telefono, saldo);   // creamos el objeto de cliente (modelo)
+        
+        int registrosInsertados = new ClienteDaoJDBC().insertar(cliente);   // insertamos el nuevo objeto en la base de datos
+        System.out.println("Registros Insertados: " + registrosInsertados); // la variable no era necesario definirla pero es para ver por consola los registros insertados
+        
+        this.accionDefault(request, response);  // redirigimos hacia la acción por default
     }
 }
